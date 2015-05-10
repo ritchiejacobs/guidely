5a036f046cb83bebbbf2481266e3a88901214c3e
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2015-05-10 13:07:37 +0200

    add map styling + markers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,49 +1,66 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope, $ionicLoading) {
+.controller('HomeCtrl', function($scope) {
 
-  var geocoder;
+  if (document.readyState === "complete") {
+    initMap();
+  } else {
+    google.maps.event.addDomListener(window, 'load', initMap);
+  }
 
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
+  function initMap() {
+    $scope.map = map;
+
+    var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+    var styledMap = new google.maps.StyledMapType(styles);
 
     var mapOptions = {
-      center: { lat: 51.219448, lng: 51.219448},
-      zoom: 16
+      center: { lat: 51.21945, lng: 4.40246},
+      zoom: 15,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
     };
 
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
 
-    /* Option 1: Get current location with HTML5 */
-    // navigator.geolocation.getCurrentPosition(function(pos) {
-    //   map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-    //   var myLocation = new google.maps.Marker({
-    //       position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-    //       map: map,
-    //       title: "My Location"
-    //   });
-    // });
-
-    /* Option 2: Search a location */
-    geocoder.geocode( { 'address': 'antwerp'}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-
-    $scope.map = map;
+    setMarkers(map);
   }
 
-  if (document.readyState === "complete") {
-    initialize();
-  } else {
-    google.maps.event.addDomListener(window, 'load', initialize);
+  function setMarkers(map) {
+    var locations = [
+      ['Bondi Beach', 51.21945, 4.40446, 4],
+      ['Coogee Beach', 51.22399, 4.40166, 3],
+      ['Cronulla Beach', 51.21295, 4.40600, 2],
+      ['Manly Beach', 51.21545, 4.40000, 1],
+    ];
+
+    var messages = ['This', 'is', 'the', 'secret', 'message'];
+
+    for (var i = 0; i < locations.length; i++) {
+      var location = locations[i];
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(location[1], location[2]),
+          map: map,
+          icon: '../img/avatar.jpg',
+          title: location[0],
+          zIndex: location[3]
+      });
+      setMarkerMessage(marker, i);
+    }
+
+    function setMarkerMessage(marker, num) {
+      var infowindow = new google.maps.InfoWindow({
+        content: messages[num]
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(marker.get('map'), marker);
+      });
+    }
   }
 
 })
