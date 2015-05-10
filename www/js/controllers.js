5abb34fcedd745548f06044397d3a482e5b0132e
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2015-05-10 22:14:52 +0200

    add infowindow styling

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -40,33 +40,91 @@ angular.module('starter.controllers', [])
       ['Manly Beach', 51.21545, 4.40000, 1],
     ];
 
-    var messages = ['Antwerpen centrum', 'Title 2', 'Title 3', 'Title 4'];
+    var messages = [
+      {
+        'latitude': 51.21945,
+        'longitude': 4.40446,
+        'icon': '../img/avatar.jpg',
+        'name': 'Tour through Antwerp Center',
+        'location': 'Antwerp, Flanders, Belgium',
+        'rating': 5,
+        'reviewCount': '8',
+        'duration': '01:30',
+        'type': 'Cultural',
+        'guide': 'John Doe',
+        'price': '€15'
+      },
+      {
+        'latitude': 51.22399,
+        'longitude': 4.40166,
+        'icon': '../img/avatar.jpg',
+        'name': 'Best Shopping Spots Antwerp',
+        'location': 'Antwerp, Flanders, Belgium',
+        'rating': 4,
+        'reviewCount': '12',
+        'duration': '01:30',
+        'type': 'Shopping',
+        'guide': 'Billy Gates',
+        'price': '€15'
+      },
+      {
+        'latitude': 51.21295,
+        'longitude': 4.40600,
+        'icon': '../img/avatar.jpg',
+        'name': 'History of FC Antwerp',
+        'location': 'Antwerp, Flanders, Belgium',
+        'rating': 3,
+        'reviewCount': '2',
+        'duration': '00:30',
+        'type': 'Sports',
+        'guide': 'Steven Jobs',
+        'price': '€15'
+      },
+      {
+        'latitude': 51.21545,
+        'longitude': 4.40000,
+        'icon': '../img/avatar.jpg',
+        'name': 'St. Andrews Pub Crawl',
+        'location': 'Antwerp, Flanders, Belgium',
+        'rating': 5,
+        'reviewCount': '7',
+        'duration': '02:00',
+        'type': 'Nightlife',
+        'guide': 'Harry Page',
+        'price': 'free'
+      }
+    ];
 
     for (var i = 0; i < locations.length; i++) {
-      var location = locations[i];
+      var lat = messages[i].latitude;
+      var lng = messages[i].longitude;
       var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(location[1], location[2]),
+          position: new google.maps.LatLng(lat, lng),
           map: map,
-          icon: '../img/avatar.jpg',
-          title: location[0],
-          zIndex: location[3]
+          icon: messages[i].icon,
+          title: messages[i].name
       });
       setMarkerMessage(marker, i);
     }
 
     function setMarkerMessage(marker, num) {
+      var rating = '';
+      for (var i = 0; i < messages[num].rating; i++) { rating += '<i class="icon ion-android-star"></i>' }
+
       var infowindow = new google.maps.InfoWindow({
         maxWidth: 300,
         content:
-        '<div class="info">' +
-          '<h2>' + messages[num] + '</h2>' +
+        '<div class="tourinfo">' +
+          '<h1>' + messages[num].name + '</h1>' +
+          '<p>' + messages[num].location + '</p>' +
+          '<p>' + rating + ' (' + messages[num].reviewCount + ')</p>' +
           '<ul>' +
-            '<li>test</li>' +
-            '<li>test</li>' +
-            '<li>test</li>' +
-            '<li>test</li>' +
+            '<li><i class="icon ion-ios-stopwatch"></i> <b>Duration:</b> ' + messages[num].duration + '</li>' +
+            '<li><i class="icon ion-android-map"></i> <b>Type:</b> ' + messages[num].type + '</li>' +
+            '<li><i class="icon ion-ios-person"></i> <b>Guide:</b> ' + messages[num].guide + '</li>' +
+            '<li><i class="icon ion-ios-pricetag"></i> <b>Price:</b> ' + messages[num].price + '</li>' +
           '</ul>' +
-          '<button class="button button-block button-positive">Bekijk tour</button>' +
+          '<button class="button button-block button-positive">View tour</button>' +
         '</div>'
       });
 
